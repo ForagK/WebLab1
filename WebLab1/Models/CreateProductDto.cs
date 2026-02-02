ad14ca689102e44b26949de8ed0d31afa4e674b9
ForagK <148355365+ForagK@users.noreply.github.com>
2026-02-02 17:25:14 +0200

    cleanup

--- WebLab1/Models/CreateProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebLab1.Models
-{
-    //public class CreateProductDto
-    //{
-    //    public string Name { get; set; }
-    //    public float Price { get; set; }
-    //    public string? Description { get; set; }
-    //}
-}
